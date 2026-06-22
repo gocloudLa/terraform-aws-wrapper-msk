84a133dce44a5b85443c224c8187324ffe1d0928
Guido Montano <gmontano@gocloud.la>
2026-06-22 11:01:12 -0300

    fix: update error in ephemeral use

--- secretmanager.tf
+++ secretmanager.tf
@@ -1,28 +1,32 @@
-ephemeral "random_password" "scram" {
-  for_each = {
+locals {
+  scram_secrets = {
     for k, v in var.msk_parameters : k => v
     if try(v.create_scram_secret_association, var.msk_defaults.create_scram_secret_association, false) && length(try(v.scram_secret_association_secret_arn_list, var.msk_defaults.scram_secret_association_secret_arn_list, [])) == 0
   }
+}
+
+ephemeral "random_password" "scram" {
+  for_each = local.scram_secrets
 
   length  = try(each.value.scram_password_length, var.msk_defaults.scram_password_length, 32)
   special = false
 }
 
 resource "aws_secretsmanager_secret" "scram" {
-  for_each = ephemeral.random_password.scram
+  for_each = local.scram_secrets
 
-  name                    = try(var.msk_parameters[each.key].scram_secret_name, "AmazonMSK_${local.common_name}-${each.key}")
-  description             = try(var.msk_parameters[each.key].scram_secret_description, var.msk_defaults.scram_secret_description, "SCRAM credentials for MSK cluster ${each.key}")
-  kms_key_id              = try(var.msk_parameters[each.key].scram_secret_kms_key_id, var.msk_defaults.scram_secret_kms_key_id, null)
-  recovery_window_in_days = try(var.msk_parameters[each.key].scram_secret_recovery_window_in_days, var.msk_defaults.scram_secret_recovery_window_in_days, 30)
+  name                    = try(each.value.scram_secret_name, "AmazonMSK_${local.common_name}-${each.key}")
+  description             = try(each.value.scram_secret_description, var.msk_defaults.scram_secret_description, "SCRAM credentials for MSK cluster ${each.key}")
+  kms_key_id              = try(each.value.scram_secret_kms_key_id, var.msk_defaults.scram_secret_kms_key_id, null)
+  recovery_window_in_days = try(each.value.scram_secret_recovery_window_in_days, var.msk_defaults.scram_secret_recovery_window_in_days, 30)
 
-  tags = merge(local.common_tags, try(var.msk_parameters[each.key].tags, var.msk_defaults.tags, null))
+  tags = merge(local.common_tags, try(each.value.tags, var.msk_defaults.tags, null))
 }
 
 resource "aws_secretsmanager_secret_version" "scram" {
   for_each = aws_secretsmanager_secret.scram
 
-  secret_id = each.value.id
+  secret_id        = each.value.id
   secret_string_wo = jsonencode({
     username = try(var.msk_parameters[each.key].scram_username, "kafka-${each.key}")
     password = ephemeral.random_password.scram[each.key].result
